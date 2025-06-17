fe3a66bd4940b37e4e4fdab71e3460cb90f68015
nityalily <nityalily@gmail.com>
2025-06-16 20:14:39 -0400

    bru

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-import Head from 'next/head';
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div className="App gradient-anim">
